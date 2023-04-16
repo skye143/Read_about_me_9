3d23b9a5fe70c412cf725d86ebffd16b4771284d
Skye143 <skyeheredia@gmail.com>
2023-04-16 19:25:44 -0400

    added the readme look function basically the part that generates the readme

--- u/markdown.js
+++ u/markdown.js
@@ -65,3 +65,59 @@ function renderLicenseTOC(license) {
     }
 };
 
+  // Function to generate markdown for README and make it sytle of the readme a basic professional look
+  function generateMarkdown(data) {
+    return `
+    # ${data.title}
+    
+    ${renderLicenseBadge(data.license)}
+  
+    ## Table-of-Contents
+  
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    ${renderLicenseTOC(data.license)}
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+    ## [Description](#table-of-contents)
+  
+    ${data.what}
+  
+    ${data.why}
+  
+    ${data.how}
+  
+    ## [Installation](#table-of-contents)
+  
+    ${data.installation}
+  
+    ## [Usage](#table-of-contents)
+  
+    ${data.usage}
+
+    ${renderLicenseSection(data.license)}
+  
+    ## [Contributing](#table-of-contents)
+    
+    ${renderContributingSection(data.confirmContributers, data.contribute)}
+  
+    ## [Tests](#table-of-contents)
+  
+    ${data.test}
+  
+    ## [Questions](#table-of-contents)
+  
+    Please contact me using the following links:
+  
+    [GitHub](https://github.com/${data.githubUsername})
+  
+    [Email: ${data.email}](mailto:${data.email})
+  `;
+  };
+
+  module.exports = generateMarkdown;
+  
+
