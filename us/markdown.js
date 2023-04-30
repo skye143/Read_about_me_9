e73a57d8987c4e7205ce126b293ecfded674536b
Skye143 <skyeheredia@gmail.com>
2023-04-30 17:39:53 -0400

    finished assigmen

--- us/markdown.js
+++ us/markdown.js
@@ -1,123 +1,107 @@
 
 // Function/method that returns a message if user doesn't want contributors
 function renderContributingSection(confirmContributers, data) {
-    if (!confirmContributers) {
-        return `
-    Thank you for your interest in helping out; however, I will not be accepting contributions from third parties.
-      `;
-    } else {
-        return `
-    ${data}
-      `;
-    }
+  if (!confirmContributers) {
+    return `Thank you for your interest in helping out; however, I will not be accepting contributions from third parties.`;
+  } else {
+    return `${data}`;
+  }
 };
 
 // Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    if (license !== 'no license') {
-        return `
-    ![badge](https://img.shields.io/badge/license-${license}-blue)
-      `;
-    } else {
-        return ' ';
-    }
+  if (license !== 'no license') {
+    return `![badge](https://img.shields.io/badge/license-${license}-blue)`;
+  } else {
+    return '';
+  }
 };
 
 
 // Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-    if (license !== 'no license') {
-        return `
-    [${license}](https://choosealicense.com/licenses/${license})
-      `;
-    } else {
-        return ' ';
-    }
+  if (license !== 'no license') {
+    return `[${license}](https://choosealicense.com/licenses/${license})`;
+  } else {
+    return '';
+  }
 };
 
 // Function that returns the license section of the README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-    if (license !== 'no license') {
-        return `
-    ## [License](#table-of-contents)
-  
-    The application is covered under the following license:
-  
-    ${renderLicenseLink(license)}
-      `;
-    } else {
-        return ' ';
-    }
+  if (license !== 'no license') {
+    return `## [License](#table-of-contents)
+The application is covered under the following license:
+${renderLicenseLink(license)}`;
+  } else {
+    return '';
+  }
 };
 
 // Function that returns license in table of contents
 // If there is no license, return an empty string
 function renderLicenseTOC(license) {
-    if (license !== 'no license') {
-        return `
-    * [License](#license)
-      `;
-    } else {
-        return ' ';
-    }
+  if (license !== 'no license') {
+    return `* [License](#license)`;
+  } else {
+    return '';
+  }
 };
 
-  // Function to generate markdown for README and make it sytle of the readme a basic professional look
-  function generateMarkdown(data) {
-    return `
-    # ${data.title}
-    
-    ${renderLicenseBadge(data.license)}
-  
-    ## Table-of-Contents
-  
-    * [Description](#description)
-    * [Installation](#installation)
-    * [Usage](#usage)
-    ${renderLicenseTOC(data.license)}
-    * [Contributing](#contributing)
-    * [Tests](#tests)
-    * [Questions](#questions)
-    
-    ## [Description](#table-of-contents)
-  
-    ${data.what}
-  
-    ${data.why}
-  
-    ${data.how}
-  
-    ## [Installation](#table-of-contents)
-  
-    ${data.installation}
-  
-    ## [Usage](#table-of-contents)
-  
-    ${data.usage}
+// Function to generate markdown for README and make it sytle of the readme a basic professional look
+function generateMarkdown(data) {
+  return `# ${data.title}
 
-    ${renderLicenseSection(data.license)}
-  
-    ## [Contributing](#table-of-contents)
-    
-    ${renderContributingSection(data.confirmContributers, data.contribute)}
-  
-    ## [Tests](#table-of-contents)
-  
-    ${data.test}
-  
-    ## [Questions](#table-of-contents)
-  
-    Please contact me using the following links:
-  
-    [GitHub](https://github.com/${data.githubUsername})
-  
-    [Email: ${data.email}](mailto:${data.email})
-  `;
-  };
+${renderLicenseBadge(data.license)}
 
-  module.exports = generateMarkdown;
-  
+## Table-of-Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+${renderLicenseTOC(data.license)}
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## [Description](#table-of-contents)
+
+${data.what}
+
+${data.why}
+
+${data.how}
+
+## [Installation](#table-of-contents)
+
+${data.installation}
+
+## [Usage](#table-of-contents)
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## [Contributing](#table-of-contents)
+
+${renderContributingSection(data.confirmContributers, data.contribute)}
+
+## [Tests](#table-of-contents)
+
+${data.test}
+
+## [Questions](#table-of-contents)
+
+Please contact me using the following links:
+
+[GitHub](https://github.com/${data.githubUsername})
+
+[Email: ${data.email}](mailto:${data.email})`;
+};
+
+module.exports = generateMarkdown;
+
 
